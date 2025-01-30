90385f3f68d2be492f785d03c1c6248fdfc5fb3c
arazkuchaki <aminkuch3@gmail.com>
2025-01-30 10:37:12 +0330

    update

--- about/models.py
+++ about/models.py
@@ -4,9 +4,16 @@ from django.db import models
 
 class Level(models.Model):
     title = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title
 
 
 class Agent(models.Model):
     name = models.CharField(max_length=120)
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
+    img = models.ImageField(upload_to="agent", default="defaault.jpg")
+
+    def __str__(self):
+        return self.name
+
--- about/views.py
+++ about/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Agent, Level
 
 # Create your views here.
 
 def about(request):
-    return render(request, 'about/about.html')
+    agents = Agent.objects.filter(status=True)
+    contex = {
+        'agents':agents,
+    }
+    return render(request, 'about/about.html', context=contex)
